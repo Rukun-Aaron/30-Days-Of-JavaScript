adf7a5e3c839e8a81c91bf9b0b04a5ad0c19a8b9
BuildTools <raar518@aucklanduni.ac.nz>
2022-08-06 14:31:52 +1200

    Day 1

---
+++ 01_Day_Introduction/01_day_starter/datatypes.js
@@ -0,0 +1,22 @@
+console.table({firstName: typeof(firstName),lastName: typeof(lastName),country: typeof(country), city: typeof(city), 
+age: typeof(age), isMarried: typeof(isMarried)});
+
+
+let w;
+let x;
+let y
+const z = null;
+
+let myFirstName = "Rukun", 
+myLastName = "Aaron", 
+myMaritalStatus = false, 
+myCountry = "New Zealand", 
+myAge = 19;
+
+let firstName1 = "Rukun", lastName1 = "Aaron", maritalStatus1 = false, country1 = "New Zealand", age1 = 19;
+
+myAge = 19;
+let yourAge = 24;
+
+console.log(`I am ${myAge} years old.
+You are ${yourAge} years old.`);
--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,7 @@
 // the variable values can be accessed from other variable.js file
 console.log(firstName, lastName, country, city, age, isMarried) 
 console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+console.log(name, job, live) // comments can make code readable
+
+/* Coments can make code readable,
+easy to reuse and informative*/
